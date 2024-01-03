183f5015d11efa3e12e005cfe770fd3757e363c4
darskp <darshanpatgar100@gmail.com>
2024-01-03 19:35:10 +0530

    initial commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Emp Employee=new Emp();
+        Employee.setId(10);
+        System.out.println(Employee.getId());
+        System.out.println(Employee);
+
+    }
+}
